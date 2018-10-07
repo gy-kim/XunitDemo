34ae1d6120de0a3f2a3a930181af878a84887922
Geunyoung Kim <kgysmart@gmail.com>
2018-10-07 00:10:38 -0700

    upload project

---
+++ GameEngine/PlayerCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GameEngine
+{
+    public class PlayerCharacter
+    {
+        private int _health = 100;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public string NickName { get; set; }
+
+        public bool IsNoob { get; set; }
+
+        public PlayerCharacter()
+        {
+            IsNoob = true;
+        }
+    }
+}
