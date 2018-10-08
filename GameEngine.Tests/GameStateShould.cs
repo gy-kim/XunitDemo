25827c4203e5c137a0705b6fdbff39282c95c311
Geunyoung Kim <kgysmart@gmail.com>
2018-10-08 10:46:26 -0700

    Added Sharing Context Between Tests During Execution

---
+++ GameEngine.Tests/GameStateShould.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace GameEngine.Tests
+{
+    public class GameStateShould : IClassFixture<GameStateFixture>
+    {
+        private readonly GameStateFixture _gameStateFixture;
+        private readonly ITestOutputHelper _output;
+
+        public GameStateShould(GameStateFixture gameStateFixture, ITestOutputHelper output)
+        {
+            this._gameStateFixture = gameStateFixture;
+            this._output = output;
+        }
+
+        [Fact]
+        public void DamageAllPlayerWhenEarthquake()
+        {
+            _output.WriteLine($"GameState ID={_gameStateFixture.State.Id}");
+
+            var player1 = new PlayerCharacter();
+            var player2 = new PlayerCharacter();
+
+            _gameStateFixture.State.Players.Add(player1);
+            _gameStateFixture.State.Players.Add(player2);
+
+            var expectedHealthAfterEarchquake = player1.Health - GameState.EarthquakeDamage;
+
+            _gameStateFixture.State.Earthquake();
+
+            Assert.Equal(expectedHealthAfterEarchquake, player1.Health);
+            Assert.Equal(expectedHealthAfterEarchquake, player2.Health);
+        }
+
+        [Fact]
+        public void Reset()
+        {
+            _output.WriteLine($"GameState ID={_gameStateFixture.State.Id}");
+
+            var player1 = new PlayerCharacter();
+            var player2 = new PlayerCharacter();
+
+            _gameStateFixture.State.Players.Add(player1);
+            _gameStateFixture.State.Players.Add(player2);
+        }
+    }
+}
